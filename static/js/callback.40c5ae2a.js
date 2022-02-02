556d0c91aee1f93f1b97b458f884736fef1f3de2
Pyrrhic <nefalt2@gmail.com>
2022-02-01 21:22:34 -0600

    Updates

--- static/js/callback.40c5ae2a.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";window.location.replace(window.location.href.replace(/\/callback/,"/#/callback"))}();
-//# sourceMappingURL=callback.40c5ae2a.js.map
---
+++ static/js/callback.bee636e4.js
@@ -0,0 +1,2 @@
+!function(){"use strict";window.location.replace(window.location.href.replace(/\/callback\.html/,"/#/callback"))}();
+//# sourceMappingURL=callback.bee636e4.js.map
